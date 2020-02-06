ff096eafc57c55705312ccbaf79c5b134809cf4e
Hudson Holanda <hudsonholanda55@gmail.com>
2020-02-06 14:48:22 -0200

    adicionando monitoramento em mais de um arquivo com o watch e sincronizando com browser-sync.

--- automacao-com-gulp/05-browser-sync/gulpfile.js
+++ automacao-com-gulp/05-browser-sync/gulpfile.js
@@ -1,8 +1,10 @@
+// Adicionando dependências
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browsersync = require('browser-sync').create();
 
+// Função paraa compilar o SASS e adicionar os prefixos
 function compilaSass(){
   return gulp.src('css/scss/*.scss')
          .pipe(sass({
@@ -16,8 +18,10 @@ function compilaSass(){
          .pipe(browsersync.stream());
 }
 
+// Tarefa de gulp para a função de sass
 gulp.task('sass', compilaSass);
 
+// Função para iniciar o browser.
 function browser() {
   browsersync.init({
     server: {
@@ -26,12 +30,17 @@ function browser() {
   })
 }
 
+// Tarefa para inicar o browser-synnc
 gulp.task('browser-sync', browser);
 
+// Função de Watch do gulp
 function watch() {
   gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch(['*.html', '*.php']).on('change', browsersync.reload)
 }
 
+// Inicia a tarefa de watch
 gulp.task('watch', watch);
 
+// Tarefa padrão do gulp, que inicia o watch e o browser sync
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
